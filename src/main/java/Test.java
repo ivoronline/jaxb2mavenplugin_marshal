1a0e9e000dec44f84f9bd834174f6aaa3708d157
igoreta <ivoronline@gmail.com>
2021-05-03 13:51:47 +0200

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,22 @@
+import com.ivoronline.Person;
+
+import javax.xml.bind.*;
+import java.io.File;
+
+public class Test {
+
+  public static void main(String[] args) throws JAXBException {
+
+    //CREATE PERSON
+    Person  person = new Person();
+            person.setId  (1);
+            person.setName("John");
+            person.setAge (20);
+
+    //MARSHAL PERSON
+    Marshaller  marshaller = JAXBContext.newInstance(Person.class).createMarshaller();
+                marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+                marshaller.marshal(person, new File("src/main/resources/person.xml"));
+  }
+
+}
